b023f5a48f7b633fb78e51b685234410502a5a4e
Kong Chae Won <chaewonkong@gmail.com>
2020-01-15 21:08:08 +0900

    feat: add cors

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import * as express from "express";
+import * as cors from "cors";
 
 const PORT = process.env.PORT || 3004;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000"
+  })
+);
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello world!");
