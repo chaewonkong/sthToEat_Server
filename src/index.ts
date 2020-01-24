cd1626a8dace690e1144ff01129b3c2db4d477ab
Kong Chae Won <chaewonkong@gmail.com>
2020-01-24 15:50:25 +0900

    feat: add PORT var

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import * as express from "express";
 import { ApolloServer } from "apollo-server-express";
-
 import * as depthLimit from "graphql-depth-limit";
 import { createServer } from "http";
 import * as cors from "cors";
 import schema from "./schema";
 
+const PORT = process.env.PORT || 3004;
+
 const app = express();
+
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)]
@@ -15,7 +17,7 @@ const server = new ApolloServer({
 app.use("*", cors());
 server.applyMiddleware({ app, path: "/graphql" });
 const httpServer = createServer(app);
-httpServer.listen({ port: 3000 }, (): void =>
+httpServer.listen({ port: PORT }, (): void =>
   console.log(
     `\n🚀      GraphQL is now running on http://localhost:3000/graphql`
   )
